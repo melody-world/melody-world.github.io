33d719bd81269c6e0c3b4d79119b5445c16eb0b7
wwl-jky <jky@winwinlab.co.kr>
2023-11-17 12:56:22 +0900

    [test] 루트 경로 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 // import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import Header from './component/header';
-import Footer from './component/footer';
-import Gwangmyeong from './page/gwangmyeong';
-import News from './page/news';
+import Header from "./component/header";
+import Footer from "./component/footer";
+import Gwangmyeong from "./page/gwangmyeong";
+import News from "./page/news";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>      
-      <Header/>
+    <BrowserRouter basename="https://merry-eddy.co.kr/">
+      <Header />
       <Routes>
-        <Route exact path="/" element={<Gwangmyeong/>}/>
-        <Route path="/gwangmyeong" element={<Gwangmyeong/>}/>
-        <Route path="/news" element={<News/>}/>
+        <Route exact path="/" element={<Gwangmyeong />} />
+        <Route path="/gwangmyeong" element={<Gwangmyeong />} />
+        <Route path="/news" element={<News />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
