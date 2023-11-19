34a9a93c3e58f7cf9e07a1062f3acc97ce78d15a
eeesnghyun <szhyun2002@gmail.com>
2023-11-19 20:51:51 +0900

    [feature] 한입단어 개발

---
+++ src/page/onebiteword/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { useMediaQuery } from 'react-responsive';
+
+import styles from './onebiteword.module.scss';
+import PROJECT_LIST from 'constants/projectData';
+
+function Onebiteword() {
+
+
+}
+
+export default Onebiteword;
