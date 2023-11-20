660fdbc782553d6032cc865bf8117428c2a7313c
wwl-lsh <lsh@winwinlab.co.kr>
2023-11-20 12:33:26 +0900

    [test] 한입단어 화면 개발

--- src/page/onebiteword/index.js
+++ src/page/onebiteword/index.js
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from "react-responsive";
 
-import styles from './onebiteword.module.scss';
-import PROJECT_LIST from 'constants/projectData';
+import Request from "services/Request";
+import styles from "./onebiteword.module.scss";
 
-function Onebiteword() {
+export default function Onebiteword() {
+  const [wordList, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Request.get(`/onebiteword/1.json`);
+        const dataList = response.data.data.map((data) => ({
+          ...data,
+        }));
 
+        setData(dataList);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <div className={styles.container}>
+        <h1>단어리스트</h1>
+
+        {wordList.map((item) => (
+          <div className={styles.wordWrapper} key={item.wordSeq}>
+            <div className={styles.wordName}>{item.wordName}</div>
+            <div className={styles.wordMean} style={{ wordWrap: "break-word" }}>
+              {item.wordMean}
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
-
-export default Onebiteword;
