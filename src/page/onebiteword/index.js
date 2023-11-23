174b46fbd6378db790b8a98fe284c6316f712115
wwl-lsh <lsh@winwinlab.co.kr>
2023-11-23 12:59:28 +0900

    [test] 단어 발음 테스트

--- src/page/onebiteword/index.js
+++ src/page/onebiteword/index.js
@@ -50,6 +50,32 @@ export default function Onebiteword() {
             <div className={styles.wordMean} style={{ wordWrap: "break-word" }}>
               {item.wordMean}
             </div>
+            <div>
+              <a
+                click={() => {
+                  if (
+                    typeof SpeechSynthesisUtterance === "undefined" ||
+                    typeof window.speechSynthesis === "undefined"
+                  ) {
+                    alert("이 브라우저는 음성 합성을 지원하지 않습니다.");
+                    return;
+                  }
+
+                  window.speechSynthesis.cancel(); // 현재 읽고있다면 초기화
+
+                  const speechMsg = new SpeechSynthesisUtterance();
+                  speechMsg.rate = 1; // 속도: 0.1 ~ 10
+                  speechMsg.pitch = 1; // 음높이: 0 ~ 2
+                  speechMsg.lang = "ko-KR";
+                  speechMsg.text = item.wordName;
+
+                  // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
+                  window.speechSynthesis.speak(speechMsg);
+                }}
+              >
+                듣기
+              </a>
+            </div>
           </div>
         ))}
       </div>
