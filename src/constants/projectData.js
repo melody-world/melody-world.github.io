b82b53ada1912be7485842c35d9e0f337a14b9fc
wwl-lsh <lsh@winwinlab.co.kr>
2024-01-02 11:04:53 +0900

    [chore] 하루단어 설명 추가

--- src/constants/projectData.js
+++ src/constants/projectData.js
@@ -2,22 +2,19 @@ const PROJECT_LIST = [
   {
     id: 1,
     projectName: "광명찾자 - 아파트 실거래가 조회",
-    projectContent:
-      "국토교통부 데이터를 기반으로\n전국 아파트 실거래가를 제공합니다",
+    projectContent: "국토교통부 데이터를 기반으로\n전국 아파트 실거래가를 제공합니다",
     projectMainImage: "/images/main/img_gwangmyeong.jpg",
     projectType: ["APP", "WEB"],
     projectWebLink: "",
-    projectAndLink:
-      "https://play.google.com/store/apps/details?id=com.app.findgwangmyeong",
-    projectIosLink:
-      "https://apps.apple.com/kr/app/%EA%B4%91%EB%AA%85%EC%B0%BE%EC%9E%90/id6451822761",
+    projectAndLink: "https://play.google.com/store/apps/details?id=com.app.findgwangmyeong",
+    projectIosLink: "https://apps.apple.com/kr/app/%EA%B4%91%EB%AA%85%EC%B0%BE%EC%9E%90/id6451822761",
     readMore: "/findgwangmyeong",
     shortName: "fgm",
   },
   {
     id: 2,
     projectName: "하루단어",
-    projectContent: "해당 페이지는 준비중입니다.",
+    projectContent: "하루에 한번, 오늘의 단어를 확인해보세요!",
     projectMainImage: "/images/main/img_ready.jpg",
     projectType: ["APP", "WEB"],
     projectWebLink: "",
