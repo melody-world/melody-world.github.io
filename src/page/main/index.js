620e9afae36a5064810dcc64e85f5e1651b0148d
wwl-jky <jky@winwinlab.co.kr>
2024-02-01 11:16:09 +0900

    [fix] api 연결 수정 테스트

--- src/page/main/index.js
+++ src/page/main/index.js
@@ -15,6 +15,9 @@ export const getProjectList = async () => {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
   }).then((res) => res.json());
 
