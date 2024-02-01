41f1e3338a329c1b6ceed3c3879f2a6e59817921
wwl-jky <jky@winwinlab.co.kr>
2024-02-01 11:58:57 +0900

    [fix] api 연결 수정 테스트

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,24 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   webpack: function (config, env) {
     config.output.filename = "static/js/[name].[hash:8].js";
     config.output.chunkFilename = "static/js/[name].[hash:8].chunk.js";
